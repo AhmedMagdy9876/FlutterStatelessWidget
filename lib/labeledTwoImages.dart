686690f262c111d8fd9f4de5673e7c907f51450f
ahmed.magdy <ahmed.magdy@coretech-innovations.com>
2024-12-29 06:58:28 +0200

    some styling and refinement

--- lib/labeledTwoImages.dart
+++ lib/labeledTwoImages.dart
@@ -7,6 +7,7 @@ class LabeledTwoImages extends StatelessWidget {
       required this.firstImagePath,
       required this.secondImageUri});
 
+  // fields for the images' paths and label
   final String firstImagePath;
   final Uri secondImageUri;
   final String label;
@@ -14,24 +15,31 @@ class LabeledTwoImages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        // AppBar contains the title
         appBar: AppBar(
           title: Text('My First Project'),
         ),
         body: Center(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(8.0), // padding around page edges
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 10,
+              spacing: 10, // spacing between the images and the label
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 10,
-                  children: <Widget>[
-                    Expanded(child: Image.asset(firstImagePath)),
-                    Expanded(child: Image.network(secondImageUri.toString())),
-                  ],
+                Flexible(
+                  fit: FlexFit.loose,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 10, // spacing between images
+                    children: <Widget>[
+                      // get first image from assets
+                      Expanded(child: Image.asset(firstImagePath)),
+                      // get second image from uri
+                      Expanded(child: Image.network(secondImageUri.toString())),
+                    ],
+                  ),
                 ),
+                // the label text with styling
                 Text(label,
                     style: TextStyle(
                         fontFamily: "Suwannaphum",
